a9887bedc25b9b2337e189db9d588c363ff64008
CoffeeTimeCode <coffeetimecode@gmail.com>
2016-07-20 16:14:04 -0300

    Unity 5 Navigation System

---
+++ Assets/Inimigo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inimigo : MonoBehaviour {
+
+	private NavMeshAgent _agente;
+	public Transform _player;
+
+	// Use this for initialization
+	void Start () {
+		_agente = this.gameObject.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(this.transform.position,_player.position)>3)
+		{
+			_agente.destination = _player.position;
+		}
+	}
+}
